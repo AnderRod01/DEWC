2030fed61efcbde17f4346d62fa3301b457b3f2b
AnderRod01 <80098555+AnderRod01@users.noreply.github.com>
2022-09-15 13:05:31 +0200

    /

--- tanda2/ejercicio4/app.js
+++ tanda2/ejercicio4/app.js
@@ -1,19 +1,32 @@
-function cambiarAzul(){
-    document.body.style.backgroundColor = "blue";
-}
+let ancho = screen.width
+let altura = screen.height
 
-function cambiarRosa(){
-    document.body.style.backgroundColor = "pink";
-}
+let txt = ""
 
-function cambiarAmarillo(){
-    document.body.style.backgroundColor = "yellow";
-}
+let elep_screen = document.createElement("p");
 
-function cambiarMorado(){
-    document.body.style.backgroundColor = "purple";
-}
+elep_screen.innerHTML = "La resolución de la pantalla es "+ ancho + " x " + altura;
+document.body.appendChild(elep_screen);
 
-function cambiarVerde(){
-    document.body.style.backgroundColor = "green";
-}
+let navegador = navigator.appName
+let sistema = navigator.platform;
+let version = navigator.appVersion;
+
+
+let elep_navigator = document.createElement("p");
+elep_navigator.innerHTML ="El nombre del navegador que usas es: "+navegador + "<br>"
+    + "Usas el siguiente sistema operativo: " + sistema + "<br>"
+    + "La version del navegador que usas es: " + version ;
+
+document.body.appendChild(elep_navigator);
+
+let elep_document = document.createElement("p");
+elep_document.innerHTML ="La URL del documento es :" + document.baseURI;
+
+document.body.appendChild(elep_document);
+
+
+let elep_location = document.createElement("p");
+elep_location.innerHTML ="El protocolo usado para acceder a esta pagina ha sido :" + location.protocol;
+
+document.body.appendChild(elep_location);
