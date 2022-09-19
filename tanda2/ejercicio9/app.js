c5c62eb99d3d0884cd939d423c0c67ecb89b3035
AnderRod01 <80098555+AnderRod01@users.noreply.github.com>
2022-09-19 20:20:25 +0200

    /

--- tanda2/ejercicio9/app.js
+++ tanda2/ejercicio9/app.js
@@ -1,8 +1,23 @@
+let ventana
+
+function abrirGoogle (){
+    ventana.close;
+
+    window.open('https://www.google.es/','_self','resizable=no','scrollbars=no','toolbar=no','location=no','status=no','width=300','height=300','top='+altura/4+'','left='+ancho/2)
+}
+
 function abrirVentana(){
 
     let altura = screen.height;
     let ancho = screen.width;
 
+    let input = document.querySelector("#txtNombre");
 
-    window.open('https://www.google.es','_blank','width=300','height=300','menubar=yes','resizable=no','scrollbars=no','toolbar=no','location=no','status=no','top='+altura/4+'','left='+ancho/2).focus();
+    if (input.value != ""){
+        ventana = window.open('','_blank','resizable=no','scrollbars=no','toolbar=no','location=no','status=no','width=300','height=300','top='+altura/4+'','left='+ancho/2);
+
+        ventana.document.write ("<p>Bienvenido "+ input.value + "<br></p><input type='button' value='Empezar a navegar' onclick='abrirGoogle()'>")
+    }else{
+        alert("Debes introducir un nombre");
+    }
 }
