89a00bad683f3e27d2c9cad1666902820fc722b0
AnderRod01 <80098555+AnderRod01@users.noreply.github.com>
2022-10-20 13:18:03 +0200

    /

--- ejercicioSeguroCoche/js/appes6.js
+++ ejercicioSeguroCoche/js/appes6.js
@@ -1,6 +1,6 @@
 let importe = 2000
 let eleSel_marca
-let eleRad_tipo
+let list_eleRad_tipo
 let eleSel_anio
 let eleBtn_submit
 
@@ -11,8 +11,11 @@ let descuentoTipo = 0
 window.onload = function(){
     eleSel_marca = document.getElementById("marca")
     eleSel_anio = document.getElementById("anio")
-    eleRad_tipo = document.getElementsByName("tipo")
-    eleBtn_submit = document.getElementById("submit")
+    // list_eleRad_tipo = document.getElementsByName("tipo")
+    list_eleRad_tipo = document.getElementsByClassName("form-check-input")
+    
+    
+    eleBtn_submit = document.getElementsByTagName("button")[0];
 
     llenarAnio()
 
@@ -21,17 +24,26 @@ window.onload = function(){
 
 function prepareEvents(){
 
-    
-    if (eleRad_tipo.value == "basico"){
-        descuentoTipo = 0.3
-    }else{
-        descuentoTipo = 0.5
-    }
 
     eleBtn_submit.addEventListener('click', calcular)
 }
 
 function cambiarDescuento(){
+    console.log(document.getElementsByName("tipo").value);
+    for(let i=0; i < list_eleRad_tipo.length; i++)
+    {
+        if(list_eleRad_tipo[i].checked)
+        {
+            if (list_eleRad_tipo[i].value == "basico"){
+                descuentoTipo = 0.3
+            }else{
+                descuentoTipo = 0.5
+            }
+        }
+    }
+
+    
+
     switch (eleSel_marca.value){
         case 1:
             descuentoMarca = 0.15
@@ -49,7 +61,7 @@ function cambiarDescuento(){
 
     descuentoAnio = eleSel_anio.value
 
-    if (eleRad_tipo.value == "basico"){
+    if (list_eleRad_tipo.value == "basico"){
         descuentoTipo = 0.3
     }else{
         descuentoTipo = 0.5
