2d5ee00f88e187e49fcde969d67fba313815c1ef
AnderRod01 <80098555+AnderRod01@users.noreply.github.com>
2022-10-23 18:22:00 +0200

    ejercicio twitter

--- ejercicioTwitter/assets/js/app.js
+++ ejercicioTwitter/assets/js/app.js
@@ -4,13 +4,13 @@ let txtAreaTweet
 
 window.onload = function (){
     localStorage = window.localStorage
+    listaTweets = document.getElementById('lista-tweets')
+    txtAreaTweet = document.getElementById("tweet")
     prepareEvents ()
 }
 
 function prepareEvents (){
     document.getElementById("formulario").addEventListener("submit", tweetear)
-    listaTweets = document.getElementById('lista-tweets')
-    txtAreaTweet = document.getElementById("tweet")
 
     listaTweets.addEventListener("click", borrarTweet)
 }
@@ -22,14 +22,17 @@ function tweetear(e){
 
     let eleA_borrar = document.createElement("a")
 
-    eleA_borrar.className = "borrar-tweet"
+    let tweet = txtAreaTweet.value
+    eleA_borrar.classList = "borrar-tweet"
     eleA_borrar.innerHTML = "X"
 
     let eleLi_tweet = document.createElement("li")
-    eleLi_tweet.innerHTML =txtAreaTweet.value
+    eleLi_tweet.innerHTML =tweet
     eleLi_tweet.appendChild (eleA_borrar)
     listaTweets.appendChild(eleLi_tweet)
 
+   
+    aniadirALocalStorage(tweet)
     txtAreaTweet.value =""
 }
 
@@ -39,6 +42,23 @@ function borrarTweet (e){
           e.target.parentElement.remove();
           borrarTweetDeLocalStorage(e.target.parentElement.innerText);
      } 
+}
+
+function prepararLocalStorage (){
+    let tweets = getTweetsDeLocalStorage()
+
+    tweets.forEach(function (tweet){
+        let eleA_borrar = document.createElement("a")
+
+        eleA_borrar.classList = "borrar-tweet"
+        eleA_borrar.innerHTML = "X"
+
+        let eleLi_tweet = document.createElement("li")
+        eleLi_tweet.innerHTML =txtAreaTweet.value
+        eleLi_tweet.appendChild (eleA_borrar)
+        listaTweets.appendChild(eleLi_tweet)
+    })
+   
 }
 
 function borrarTweetDeLocalStorage (tweet){
@@ -73,3 +93,4 @@ function getTweetsDeLocalStorage (){
         lstTweets = JSON.parse (localStorage.getItem('tweets'))
     return lstTweets
 }
+
